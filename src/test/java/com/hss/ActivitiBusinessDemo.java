07746350296a92e68d42e2b66a3e3197b500d233
‘HeTreeTerr’ <3110708879@qq.com>
2021-05-11 23:57:51 +0800

    activiti进阶 1.查询流程实例列表

--- src/test/java/com/hss/ActivitiBusinessDemo.java
+++ src/test/java/com/hss/ActivitiBusinessDemo.java
@@ -6,6 +6,8 @@ import org.activiti.engine.RuntimeService;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Test;
 
+import java.util.List;
+
 public class ActivitiBusinessDemo {
 
     /**
@@ -25,5 +27,35 @@ public class ActivitiBusinessDemo {
                 .startProcessInstanceByKey("myEvection", "1002");
 //        4.输出
         System.out.println("businessKey==" + instance.getBusinessKey());
+    }
+
+    /**
+     * 查询流程实例
+     * 数据来源：
+     * ACT_RU_EXECUTION
+     */
+    @Test
+    public void queryProcessInstance(){
+//        1.定义流程key
+        String processDefinitionKey = "myEvection";
+//        2.获取流程引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        3.获取RuntimeService
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+//        4.获取流程实例列表
+        List<ProcessInstance> list = runtimeService
+                .createProcessInstanceQuery()
+                .processDefinitionKey(processDefinitionKey)
+                .list();
+//        5.输出
+        for(ProcessInstance processInstance : list){
+            System.out.println("--------------------");
+            System.out.println("流程实例id:" + processInstance.getProcessInstanceId());
+            System.out.println("所属流程定义id:" + processInstance.getProcessInstanceId());
+            System.out.println("是否执行完成:" + processInstance.isEnded());
+            System.out.println("是否暂停:" + processInstance.isSuspended());
+            System.out.println("是否活动标识:" + processInstance.getActivityId());
+            System.out.println("BusinessKey:" + processInstance.getBusinessKey());
+        }
     }
 }
