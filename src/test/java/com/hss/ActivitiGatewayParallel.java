cfc52c0536a531e3e82dedb35dc7b53301399414
‘HeTreeTerr’ <3110708879@qq.com>
2021-06-08 23:43:36 +0800

    activiti进阶 1.并行网关，流程定义，以及任务查询和办理

--- src/test/java/com/hss/ActivitiGatewayParallel.java
+++ src/test/java/com/hss/ActivitiGatewayParallel.java
@@ -1,10 +1,14 @@
 package com.hss;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.RepositoryService;
+import com.hss.pojo.Evection;
+import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 并行网关测试(无视自定义的规则，要求所有的分支都必须执行完毕，才会执行下一步任务)
@@ -39,6 +43,43 @@ public class ActivitiGatewayParallel {
     public void startProcessInstance(){
 //        1.获取流程引擎
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        2.获取runtimeService
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+//        3.启动流程实例
+        Map<String,Object> map = new HashMap<>();
+        Evection evection = new Evection();
+        evection.setNum(4D);
+        map.put("evection",evection);
+        ProcessInstance processInstance = runtimeService
+                .startProcessInstanceByKey("evection-parallel", "1018", map);
+//        4.输出
+        System.out.println("流程实例id=" + processInstance.getId());
+        System.out.println("流程实例key=" + processInstance.getProcessDefinitionKey());
+        System.out.println("流程实例商业key=" + processInstance.getBusinessKey());
+    }
 
+    /**
+     * 任务办理
+     */
+    @Test
+    public void completeTask(){
+//        1.获取流程引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        2.获取taskService
+        TaskService taskService = processEngine.getTaskService();
+//        3.查询任务
+        String assignee = "Mr.li";
+        Task task = taskService.createTaskQuery()
+                .processDefinitionKey("evection-parallel")
+                .processInstanceBusinessKey("1018")
+                .taskAssignee(assignee)
+                .singleResult();
+        if(null != task){
+            System.out.println(task.getName());
+            taskService.complete(task.getId());
+            System.out.println("办理成功！");
+        }else{
+            System.out.println("没有查询到相关信息！");
+        }
     }
 }
