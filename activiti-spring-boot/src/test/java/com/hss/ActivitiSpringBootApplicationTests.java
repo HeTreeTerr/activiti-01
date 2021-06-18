2d33db918a29029a9419bc2dbcf104071e12db36
hesensen <hess@erongdu.com>
2021-06-18 18:28:14 +0800

    activi整合spring-boot： 代码测试：启动时自动生成数据库，自动部署流程。

--- activiti-spring-boot/src/test/java/com/hss/ActivitiSpringBootApplicationTests.java
+++ activiti-spring-boot/src/test/java/com/hss/ActivitiSpringBootApplicationTests.java
@@ -8,6 +8,10 @@ import org.activiti.api.process.model.builders.ProcessPayloadBuilder;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.api.task.model.Task;
+import org.activiti.api.task.model.builders.GetTasksPayloadBuilder;
+import org.activiti.api.task.model.builders.TaskPayloadBuilder;
+import org.activiti.api.task.model.payloads.GetTasksPayload;
 import org.activiti.api.task.runtime.TaskRuntime;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
@@ -70,5 +74,39 @@ public class ActivitiSpringBootApplicationTests {
         log.info("流程实例:id={}",processInstance.getId());
         log.info("流程实例:key={}",processInstance.getProcessDefinitionKey());
         log.info("流程实例:businessKey={}",processInstance.getBusinessKey());
+    }
+
+    /**
+     * 查询任务并完成自己的任务
+     */
+    @Test
+    public void testTask(){
+//        1.模拟用户登录
+        securityUtil.logInAs("jack");
+//        2.分页查询待办任务
+        Page<Task> taskPage = taskRuntime.tasks(Pageable.of(0, 10));
+//        3.判断并输出
+        if(taskPage.getTotalItems() > 0){
+            for(Task task : taskPage.getContent()){
+//                输出
+                log.info("task:id={}",task.getId());
+                log.info("task:name={}",task.getName());
+//                拾取任务
+                taskRuntime.claim(TaskPayloadBuilder
+                        .claim()
+                        .withTaskId(task.getId())
+                        .build()
+                );
+//                办理任务
+                taskRuntime.complete(TaskPayloadBuilder
+                        .complete()
+                        .withTaskId(task.getId())
+                        .build()
+                );
+                log.info("");
+            }
+        }else{
+            log.info("没有查找到相关信息！");
+        }
     }
 }
