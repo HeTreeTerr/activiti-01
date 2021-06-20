7471811a9933ad41a2a6fe0788b57f31c56f2fb6
‘HeTreeTerr’ <3110708879@qq.com>
2021-06-21 00:02:54 +0800

    activiti-springboot-实战演练： 1.补充jar包（jdbc、mybatis、activiti7） 2.添加配置

--- act-springboot-demo/src/main/java/com/hss/ActSpringbootDemoApplication.java
+++ act-springboot-demo/src/main/java/com/hss/ActSpringbootDemoApplication.java
@@ -1,9 +1,11 @@
 package com.hss;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hss.mapper")
 public class ActSpringbootDemoApplication {
 
     public static void main(String[] args) {
