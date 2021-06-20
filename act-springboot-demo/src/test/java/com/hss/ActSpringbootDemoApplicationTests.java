5ba4c675c502d6de62dbe78b01a8cb353e454b91
‘HeTreeTerr’ <3110708879@qq.com>
2021-06-20 23:33:01 +0800

    activiti-springboot-实战演练 1.项目初始化

---
+++ act-springboot-demo/src/test/java/com/hss/ActSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
